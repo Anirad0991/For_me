d8c4aa6a13a2f3b262d7b40dc0b44a838a60d0a2
Anirad0991 <ottilya666@gmail.com>
2022-09-27 21:08:26 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,23 +22,29 @@ public class Main {
 //            }
 //        }
 
-        int x = 3;
-        while (x > 0) {
-            if (x > 2) {
-                System.out.print("a");
+
+        class Dog {
+            int size;
+            String breed;
+            String name;
+
+            void bark() {
+                System.out.println("Гав! Ваф!");
             }
-            x = x - 1;
-            System.out.print("-");
-            if (x == 2) {
-                System.out.print("b c");
-            }
-            x = x - 1;
-            System.out.print("-");
-            if (x == 1) {
-                System.out.print("d");
-            }
-            x = x - 1;
         }
+
+        class DogRestDrive {
+            public static void main (String[] args) {
+                Dog d = new  Dog();
+                d.size = 40;
+                d.bark();
+                System.out.println(d.size);
+            }
+        }
+
     }
+
 }
+
+
 
