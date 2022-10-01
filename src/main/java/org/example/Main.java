38cb63d86b0d51b495f5412bbbd1c81cf2911fb3
Anirad0991 <ottilya666@gmail.com>
2022-10-01 18:07:51 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-   int x = 5;
-   int y = 9;
-   int z = x + y;
+        int x = 5;
+        int y = 9;
+        int z = x + y;
         System.out.println(z);
-   double a = 7.5;
-   double b = a * 2;
-   double c = a * 3;
+        double a = 7.5;
+        double b = a * 2;
+        double c = a * 3;
         System.out.println(b);
         System.out.println(c);
-   int d = 2;
-   int e = 8;
-   int v = d * 10 + e;
+        int d = 2;
+        int e = 8;
+        int v = d * 10 + e;
         System.out.println(v);
-   int l = 74;
-   int i = l / 3;
+        int l = 74;
+        int i = l / 3;
         System.out.println(i);
 
         String new1 = "Hello world!";
@@ -39,35 +39,39 @@ public class Main {
         System.out.print(new5);
     }
 }
+
 class targil2 {
     static Scanner reader = new Scanner(System.in);
+
     public static void main(String[] args) {
         int a = reader.nextInt();
-        System.out.println("aa="+(a*a));
-        System.out.println("a10="+(a*10));
+        System.out.println("aa=" + (a * a));
+        System.out.println("a10=" + (a * 10));
     }
 }
 
 class targil3 {
     static Scanner reader = new Scanner(System.in);
-    public static void main(String[]args) {
+
+    public static void main(String[] args) {
         int a = reader.nextInt();
-        System.out.println(" "+(a));
-        System.out.println(" "+(a+1));
-        System.out.println(" "+(a-2));
+        System.out.println(" " + (a));
+        System.out.println(" " + (a + 1));
+        System.out.println(" " + (a - 2));
     }
 }
 
-class  targil {
-    static  Scanner reader = new Scanner(System.in);
-    public  static void main(String[] args){
+class targil {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
         int a = reader.nextInt();
-        System.out.println(a-10*a);
+        System.out.println(a - 10 * a);
     }
 }
 
 class exempel2_8 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int x = 6;
         System.out.println(x);
         System.out.println(x * x);
@@ -76,7 +80,7 @@ class exempel2_8 {
 }
 
 class exempel2_9 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int x = 9;
         System.out.println("Значение x равно " + x);
         System.out.println("Квадрат x равен " + (x * x));
@@ -84,17 +88,17 @@ class exempel2_9 {
 }
 
 class exempel2_10 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int x = 2;
         int y = 5;
         System.out.println("Значение переменной х равно " + x + " Значение переменной у равно " + y);
-        System.out.println("Поизведение переменных х и у равна " + (x * y) );
+        System.out.println("Поизведение переменных х и у равна " + (x * y));
         System.out.println("Сумма переменных х и у равна " + (x + y));
     }
 }
 
 class exempel2_11 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String name = "Дарина Катык";
         String number = "+79788750317";
         System.out.println("****************");
@@ -105,7 +109,7 @@ class exempel2_11 {
 }
 
 class exempel2_12 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("******");
         System.out.println(" ****");
         System.out.println("  **");
@@ -115,7 +119,7 @@ class exempel2_12 {
 }
 
 class exempel2_13 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int x = 5;
         int y = 9;
         int z = x + y;
@@ -129,43 +133,29 @@ class exempel2_13 {
     }
 }
 
-class  exempel2_14 {
-    static  Scanner reader = new Scanner(System.in);
-    public  static void main(String[] args){
+class exempel2_14 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
         int a = reader.nextInt();
-        System.out.println(a+10);
+        System.out.println(a + 10);
     }
 }
-class  exempel2_15 {
-    static  Scanner reader = new Scanner(System.in);
-    public  static void main(String[] args){
+
+class expel2_15 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
         int a = reader.nextInt();
-        System.out.println(a*10);
+        System.out.println(a * 10);
     }
 }
 
 // 29.09.2022 книга глава 5 ст 125, задачи 2.16 ст. 12
 
-
+/*
 class SeeWar {
 
-    public String checkYourself(String stringGuess) {
-        int guess = Integer.parseInt(stringGuess);
-        String result = "Мимо";
-
-        for (int cell : locationCells){
-            if(guess == cell){
-                result = "Попал";
-                numOfHits++;
-                break;
-            }
-        }
-        if(numOfHits == locationCells.length){
-            result = "Потопил";
-        }
-        System.out.println(result);
-        return result;
-    }
     public class SimpleDotComTestDrive{
         public static void main (String[] args){
             SimpleDotCom dot = new SimpleDotCom();
@@ -221,19 +211,237 @@ class SeeWar {
             theDotCom.setLocationCells(location);
             boolean isAlive = true;
 
-            while (isAlive == true){
+            while (isAlive == true) {
                 String guess = helper.getUserInputs("Введите число");
                 String result = theDotCom.checkYourself(guess);
                 numOfGuesses++;
-                if(result.equals("Потопил")){
+                if (result.equals("Потопил")) {
                     isAlive = false;
                     System.out.println("Вам потребовалось " + numOfGuesses + " попыток(и)");
                 }
             }
+
+
         }
     }
 }
+ */
+
 // страница 142 нужно добавить код про helper
+
+class example2_16 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        System.out.println(x + 6);
+        System.out.println(x - 12);
+    }
+}
+
+class example2_17 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        System.out.println(x * x);
+    }
+}
+
+class example2_18 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        System.out.println(x * x + "&" + x * x * x);
+    }
+}
+
+class example2_19 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        System.out.println((x - 1) + " " + x + " " + (x + 1));
+    }
+}
+
+class example2_20 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        if (x > 0) {
+            int y = Math.abs(x) * -1;
+            System.out.println(x);
+            System.out.println(y);
+        } else if (x < 0) {
+            int y = Math.abs(x);
+            System.out.println(x);
+            System.out.println(y);
+        } else {
+            System.out.println(x);
+            System.out.println(x);
+        }
+    }
+}
+
+class example2_21 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        int y = x + 1;
+        int z = y + 1;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
+
+class example2_22 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        int y = reader.nextInt();
+
+        System.out.println(x + y);
+        System.out.println(x * y);
+        System.out.println(x - y);
+        System.out.println(y - x);
+    }
+}
+
+class example2_23 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        int y = reader.nextInt();
+
+        System.out.println(x * x);
+        System.out.println(y * y);
+        System.out.println((x * x) + (y * y));
+    }
+}
+
+class example2_24 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        int y = reader.nextInt();
+
+        System.out.println(x * x);
+        System.out.println(y * y);
+        System.out.println((x * x) + (y * y));
+    }
+}
+
+class example2_25 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        int y = reader.nextInt();
+        int z = reader.nextInt();
+
+        System.out.println("Объем параллелепипеда = " + (x * y * z));
+        System.out.println("Площадь каждой грани параллелепипеда = " + (x * y) + " " + (z * y) + " " + (z * x));
+        System.out.println("Общая площадь параллелепипеда = " + ((x * y) * 2 + (z * y) * 2 + (x * z) * 2));
+        System.out.println("Общий периметр параллелепипеда = " + ((x * 4) + (4 * y) + (4 * z)));
+    }
+}
+
+class example2_26 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int age = reader.nextInt();
+        int year = reader.nextInt();
+
+        if (year > age) {
+            System.out.println("Твой возраст " + (year - age));
+        } else {
+            System.out.println("Ты еще не родился)))");
+        }
+    }
+}
+
+class example2_27 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double milliGrams = reader.nextInt();
+
+        System.out.println(milliGrams / 1000);
+
+    }
+}
+
+class example2_28 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double kab = reader.nextInt();
+        double metr = kab * 185.2;
+        double kilometr = metr / 1000;
+        if (kab >= 0) {
+            System.out.println("Метров " + metr);
+            System.out.println("Километров " + kilometr);
+        } else {
+            System.out.println("Вы ввели отрицательное число");
+        }
+    }
+}
+
+class example2_29 {
+    static Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        double hour = reader.nextDouble();
+        double sekunda = hour * 3600;
+        if (hour >= 0) {
+            System.out.println(sekunda);
+        } else {
+            System.out.println("Отрицательного времени не существует");
+        }
+    }
+}
+
+class example2_30 {
+    static Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        int hour = reader.nextInt();
+        int min = reader.nextInt();
+
+        if (hour <= 23 & hour >= 0 || min <= 59 & min >= 0) {
+            int secFromMid = hour * 3600 + min * 60;
+            int minFromMid = secFromMid / 60;
+
+            int secToMid = 86440 - secFromMid;
+            int minToMid = 1440 - minFromMid;
+
+            System.out.println("Секунд прошло с полуночи " + secFromMid + ". Минут прошло с полуночи " + minFromMid);
+            System.out.println("Секунд до полуночи " + secToMid + ". Минут до полуночи " + minToMid);
+        }else {
+            System.out.println("Значение не попадает в диапазон часов и минут");
+        }
+    }
+}
+
+// Задачи 2.31, использование библиотеки java
+
+
+
+
+
+
+
+
+
 
 
 
