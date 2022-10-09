8d0d97e5d436d3585c90a28a7bb89943d3f2e8c3
Anirad0991 <ottilya666@gmail.com>
2022-10-09 20:47:12 +0300

    1

--- src/main/java/org/example/Example3.java
+++ src/main/java/org/example/Example3.java
@@ -34,3 +34,77 @@ class Example3_3 {
 
 // закончила с задачами на сторанице 19, книгу читай со страницы 189,я сегодня не осилила чтение
 // читай книгу со страницы 222, задачи не трогала, продолжай третий раздел
+
+class Example3_4 {
+    public static Scanner in = new Scanner(System.in); // метод для пересчета валюты
+    public static void main (String[] args){ //
+        double valuta1 = in.nextDouble(); // принимаем сумму первой валюты
+        double coefficient = in.nextDouble(); // банковский коэфициент
+
+        double valuta2 = valuta1 * coefficient;// пересчет в валюту два
+        System.out.println(valuta1 + " долларов равно " + valuta2 + " рублей");
+    }
+}
+class Example3_5 {
+    public static Scanner in = new Scanner(System.in); // метод площадь и периметр прямоугольного треугольника
+    public static void main (String[] args){
+        double a = in.nextDouble(); // катет
+        double b = in.nextDouble(); // катет
+        double c = in.nextDouble(); // гипотенуза
+
+        double p = a + b + c; // периметр треугольника
+        double s = (a * b) / 2 ; // площадь треугольникиа
+        System.out.println("Периметр равен " + p + " Площадь равна " + s);
+    }
+}
+class Example3_6 {
+    public static Scanner in = new Scanner(System.in); // три следующие числа с шагом в две единицы
+    public static void main (String[] args){
+        int a = in.nextInt();
+        int b = a + 2;
+        int c = b + 2;
+        int d = c + 2;
+
+        System.out.println(b + ", " + c + ", " + d);
+
+    }
+}
+class Example3_7 {
+    public static Scanner in = new Scanner(System.in); // три предыдущих числа
+    public static void main (String[] args){
+        int a = in.nextInt();
+        int b = a - 1;
+        int c = b - 1;
+        int d = c - 1;
+
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
+class Example3_9 {
+    public static Scanner in = new Scanner(System.in); // васины счастливые крышки
+    public static void main (String[] args){
+        int a = in.nextInt(); // крышки 10 б
+        int b = in.nextInt();// крышки 100 б
+        int c = in.nextInt();// крышки 1000 б
+
+        int d, e, f;
+        d = 10; // крышка стоит 10 баллов
+        e = 100; // крышка стоит 100 баллов
+        f = 1000; // крышка стоит 1000 баллов
+
+
+        System.out.println("Общее количество баллов Васи равно " + ((a*d) + (b*e) + (c*f)));
+
+    }
+}
+class Example3_10 {
+    public static Scanner in = new Scanner(System.in); // Вася едет за границу
+    public static void main (String[] args){
+        double a = in.nextDouble(); // Васины деньги в рублях
+        double b = in.nextDouble();// Дали родители в рублях
+        double c = in.nextDouble();// курс евро
+        System.out.println("Общее количество денег Васи равно " + ((a + b) / c));
+    }
+}
