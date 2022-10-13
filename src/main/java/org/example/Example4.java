e1eaf8e22564abfe01bf6e9b3a1d523da331144a
Anirad0991 <ottilya666@gmail.com>
2022-10-13 20:07:04 +0300

    1

--- src/main/java/org/example/Example4.java
+++ src/main/java/org/example/Example4.java
@@ -4,36 +4,21 @@ import java.util.Scanner;
 
 /*
 public class Example4 {
-    int a, b, c, d, f;
-    public static Scanner in = new Scanner(System.in);
-
-    public void Example4_1() {
-        int mod = in.nextInt(); // принимаем двухзначное число
-        if (mod >=10 && mod <100) {
-            int a = mod / 10; // делим двухзначное число на 10 и получаем первую цифру
-            int b = mod % 10; // делим на 10 по модулю и получаем остаток
-            System.out.println(a + " " + b); // выводим результат через пробел
-        } else {
-            System.out.println("Число не подходит под описание");
-        }
-    }
-
-    public void jump() {
-        System.out.println("Прыг-скок!");
-    }
 
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println(getTwoNumberReverse(x));
 
-      Cat barsik = new Cat();
-        barsik.age = 3;
-        barsik.name = "Барсик";
+    }
 
-        barsik.sayMeow();
-        barsik.jump();
-
-
-
+    public static int getTwoNumberReverse(int a) { // двухзначное число в обратном порядке
+            int c = a / 10; // делим двухзначное число на 10 и получаем первую цифру
+            int b = a % 10; // делим на 10 по модулю и получаем остаток
+            String st = b + "" + c;
+            a = Integer.parseInt(st);
+            return a;
     }
 }
 */
@@ -313,7 +298,83 @@ class Example4_15 {
     }
 
     // книга на чтранице 72 подзагловок се объекты..., задачи 4_16, перечитай свои комментарии бери следующие два понятия
-// книга страница 88
+class Example4_16 {
+        public static Scanner in = new Scanner(System.in); /* Назовем «перестановкой» операцию над числом, при которой его первая и последняя цифры меняются местами. Например, из числа 1234
+получается число 4231. Напишите программу, которая принимает с
+клавиатуры тр */
+        public static void main (String[] args){ //
+            int mod = in.nextInt(); // принимаем трехзначное число
+            if (mod >=100 && mod <999) {
+                int a = mod / 100; // делим без остатка на 100 получаем первую цифру
+                int b = (mod % 100 ) / 10; //делим без остатка на 100 и остаток на 10
+                int c = mod % 10; // модулем выясняем последнюю цифру
+                System.out.println(c + "" + b + "" +a); // выводим результат через пробел
+            } else {
+                System.out.println("Число не подходит под описание");
+            }
+        }
+    }
+
+class Example4_17 {
+    public static Scanner in = new Scanner(System.in); //
+
+    public static void main(String[] args) { //
+        int mod = in.nextInt(); // принимаем четырех число
+        if (mod > 0) {
+            int x = mod / 1000;// выясняем первую цифру
+            int a = (mod % 1000) / 100; // делим по модулю
+            int b = (mod % 100) / 10; //
+            int c = mod % 10; // модулем выясняем последнюю цифру
+
+            System.out.println(c + "" + a + "" + b + "" + x); // выводим на экран перестроинное число
+        } else {
+            System.out.println("Число не подходит под описание");
+        }
+
+    }
+}
+
+class Example4_18 {
+    public static Scanner in = new Scanner(System.in); /*Назовем «линейным сдвигом числа влево» операцию, при которой его
+цифры перемещаются на одну позицию влево (единицы - на место
+десятков, десятки - на место сотен, сотни - на место тысяч и так далее) - при это
+     */
+    public static void main (String[] args){ //
+        int mod = in.nextInt(); // принимаем четырех число
+        if (mod > 0) {
+            int x = mod / 1000;// выясняем первую цифру
+            int a = (mod % 1000) / 100; // делим по модулю
+            int b = (mod % 100) / 10; //
+            int c = mod % 10; // модулем выясняем последнюю цифру
+            String st = a + "" + b + "" + c + "" + 0;
+            int y = Integer.parseInt(st);
+            System.out.println(y); // выводим на экран перестроинное число
+        } else {
+            System.out.println("Число не подходит под описание");
+        }
+    }
+}
+
+class Example4_19 {
+    public static Scanner in = new Scanner(System.in); /* Kbytqysq cldbu lkz nht[pyfxyjuj xbckf */
+    public static void main (String[] args){ //
+        int mod = in.nextInt(); // принимаем трехзначное число
+        if (mod >=100 && mod <999) {
+            int a = mod / 100; // делим без остатка на 100 получаем первую цифру
+            int b = (mod % 100 ) / 10; //делим без остатка на 100 и остаток на 10
+            int c = mod % 10; // модулем выясняем последнюю цифру
+            String st = b + "" + c + "" + 0;
+            int y = Integer.parseInt(st);
+            System.out.println(y); // выводим на экран перестроинное число
+        } else {
+            System.out.println("Число не подходит под описание");
+        }
+    }
+}
+
+
+
+// книга страница 88, задачи ст 25, попробуй начать создавать класс и методы что-бы все переоформить и работать в рамках класса и методов, новые понятия из таблицы
 
 
 
